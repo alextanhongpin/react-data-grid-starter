1d9f2db00591bfb19e1929fa20939ca5095708e5
alextanhongpin <alextan220990@gmail.com>
2021-08-06 17:44:01 +0800

    chore: add drag n drop and jsondiff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 import ReactDataGrid, { TextEditor, SelectColumn } from "react-data-grid";
+import { useDropzone } from "react-dropzone";
+import Papa from "papaparse";
+import jsonDiff from "json-diff";
 
-const columns = [
+const defaultColumns = [
   SelectColumn,
   {
     key: "id",
@@ -27,29 +30,73 @@ const columns = [
 ];
 
 function App() {
+  const [columns, setColumns] = useState(defaultColumns);
   const [rows, setRows] = useState([
     { id: 0, title: "row1", count: 20, valid: false },
     { id: 1, title: "row2", count: 100, valid: true },
     { id: 2, title: "row3", count: 40, valid: true },
     { id: 3, title: "row4", count: 30, valid: true },
   ]);
+  const [originalRows, setOriginalRows] = useState([]);
   const [selectedRows, setSelectedRows] = useState();
 
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      Papa.parse(file, {
+        header: true,
+        complete: function (results) {
+          setOriginalRows(results.data);
+          setRows(results.data);
+          setColumns([
+            SelectColumn,
+            ...results.meta.fields.map((key) => ({
+              key,
+              name: key,
+              editor: TextEditor,
+            })),
+          ]);
+          console.log(results);
+        },
+      });
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <ReactDataGrid
-      minHeight={150}
-      defaultColumnOptions={{
-        sortable: true,
-        resizable: true,
-      }}
-      columns={columns}
-      rows={rows}
-      rowKeyGetter={(row) => row.id}
-      onRowsChange={setRows}
-      selectedRows={selectedRows}
-      onSelectedRowsChange={setSelectedRows}
-      rowClass={(row) => (row.valid ? undefined : "error")}
-    />
+    <div>
+      <h1>Grid example</h1>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+          <p>Drop the files here...</p>
+        ) : (
+          <p>Drag n drop some files here, or click to select files</p>
+        )}
+      </div>
+
+      <ReactDataGrid
+        minHeight={150}
+        defaultColumnOptions={{
+          sortable: true,
+          resizable: true,
+        }}
+        columns={columns}
+        rows={rows}
+        rowKeyGetter={(row) => row.id}
+        onRowsChange={setRows}
+        selectedRows={selectedRows}
+        onSelectedRowsChange={setSelectedRows}
+        rowClass={(row) => (row.valid ? undefined : "error")}
+      />
+      <code
+        style={{
+          "white-space": "break-spaces",
+        }}
+      >
+        {jsonDiff.diffString(originalRows, rows, { color: false })}
+      </code>
+    </div>
   );
 }
 
